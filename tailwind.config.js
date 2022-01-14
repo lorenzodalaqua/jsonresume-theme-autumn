6005858f7bd6ad6a29befacdd5d9896140908a4c
Lorenzo Dal'Aqua <lorenzodalaqua@gmail.com>
2022-01-14 20:23:03 -0300

    Add default font 'Ubuntu'
    Add comments for clarity on main template file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ const { VIEWS_DIR } = require('./build-config');
 module.exports = {
   content: [`${VIEWS_DIR}/**/*.hbs`],
   theme: {
+    fontFamily: {
+      sans: ['Ubuntu', 'ui-sans-serif', 'sans-serif']
+    },
     extend: {}
   },
   plugins: []
